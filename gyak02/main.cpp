6d9d9469d645c22a8bb1e88ae200395ee075a07f
Adam Csapo <csapo.adam@gmail.com>
2019-09-18 17:56:52 +0200

    major flaw: no destructor! heap allocations accumulate wo/ memory being freed

--- gyak02/main.cpp
+++ gyak02/main.cpp
@@ -6,13 +6,23 @@
 
 #include "linkedlist.h"
 
-int main()
-{
+void f() { // szimulaljuk a main()-tol eltero fv-ek hasznalatat
 	LinkedList my_ll;
 	my_ll.addNode(5);
 	my_ll.addNode(6);
 	my_ll.addNode(7);
 	my_ll.print();
+}
+
+
+int main()
+{
+	for (int i = 0; i < 10000; i++) {
+		f();
+		if (i % 100 == 0) {
+			std::cin.get();
+		}
+	}
 
 	// ha Node kulon osztaly letrehozhatjuk, de nem sok mindent tudunk vele csinalni
 	// ha raadasul privat beagyazott, nem tudjuk peldanyositani kivulrol!
@@ -23,3 +33,4 @@ int main()
 	std::cin.get();
     return 0;
 }
+
