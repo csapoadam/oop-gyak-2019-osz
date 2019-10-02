d78db9c185f4beebaffa281b7d6257e06d3d2f83
Adam Csapo <csapo.adam@gmail.com>
2019-10-02 17:20:46 +0200

    file types

---
+++ gyak04/filesystem.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+
+class Node {
+	std::string name;
+	int sizeInBytes;
+public:
+	Node(std::string name, int sz) : name(name), sizeInBytes(sz) { }
+	void print() {
+		std::cout << "I am node w/ name: " << name << std::endl;
+	}
+	std::string getName() { return name; }
+	int getSize() { return sizeInBytes; }
+};
+
+class TextNode : public Node {
+public:
+	TextNode(std::string name, int sz) : Node(name, sz) {}
+	void print() {
+		std::cout << "I am a TextNode w/ name " << getName() << " and size " << getSize() << " bytes" << std::endl;
+	}
+};
+
+class ImageNode : public Node {
+	int height, width;
+public:
+	ImageNode(std::string name, int sz, int w, int h) : Node(name, sz) {
+		height = h;
+		width = w;
+	}
+	void print() {
+		std::cout << "I am an ImageNode w/ name " << getName() << ", size " << getSize() << " bytes";
+		std::cout << " and dimensions " << width << " x " << height << std::endl;
+	}
+};
---
+++ gyak04/main.cpp
@@ -0,0 +1,26 @@
+// FileSystem.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "filesystem.h"
+
+
+int main()
+{
+  
+  TextNode t1 = TextNode("MyTextFile1.txt", 30);
+  t1.print();
+  ImageNode i1 = ImageNode("MyImage.jpg", 650, 200, 150);
+  i1.print();
+  ImageNode i2 = ImageNode("MyImage2.jpg", 6500, 250, 150);
+  i2.print();
+
+  DocumentStore ds;
+  ds.addDocument(&t1);
+  ds.addDocument(&i1);
+  ds.addDocument(&i2);
+
+  std::cin.get();
+    return 0;
+}
+
