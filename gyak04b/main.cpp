3a2e431f90d673150b092cade2836e1a243eb951
Adam Csapo <csapo.adam@gmail.com>
2019-10-02 19:10:26 +0200

    correction

--- gyak04b/main.cpp
+++ gyak04b/main.cpp
@@ -1,39 +1,36 @@
-#pragma once
+// VehicleClasses.cpp : Defines the entry point for the console application.
+//
 
-#include <string>
-#include <vector>
+#include "stdafx.h"
+#include <iostream>
 
-class Vehicle {
-  std::string name;
-public:
-  Vehicle(std::string n) : name(n) {}
-  virtual void print() = 0; // mivel van legalabb 1 virtualis fv ami 0, ezert absztrakt osztaly - nem peldanyosithato
-  std::string getName() { return name; }
-};
+#include "vehicles.h"
 
-class Car : public Vehicle {
-public:
-  Car(std::string n) : Vehicle(n) {}
-  void print() { std::cout << "Car named " << getName() << std::endl; }
-};
+int main()
+{
+  // Vehicle v;
+  // absztrakt osztaly, nem megy!!
+  Vehicle* c1 = new Car("Oliver's Ferrari");
+  Vehicle* c2 = new Car("Erno's Porsche");
+  Vehicle* c3 = new Car("Adam's Mercedes");
+  Vehicle* mb1 = new MotorBike("Mate's Harley");
 
-class MotorBike : public Vehicle {
-public:
-  MotorBike(std::string n) : Vehicle(n) {}
-  void print() { std::cout << "Motorbike named " << getName() << std::endl; }
-};
+  VehiclePark vehiclepark;
+  vehiclepark.addVehicle(c1);
+  vehiclepark.addVehicle(c2);
+  vehiclepark.addVehicle(mb1);
+  vehiclepark.printVehicles();
 
-class VehiclePark {
-  std::vector<Vehicle*> vehiclepark;
-public:
-  VehiclePark() {}
-  void addVehicle(Vehicle* vp) {
-    vehiclepark.push_back(vp);
-  }
-  void printVehicles() {
-    std::cout << "Vehicles in park are :" << std::endl;
-    for (Vehicle* vp : vehiclepark) {
-      vp->print();
-    }
-  }
-};
+  /*
+  Tegyuk fel, hogy itt van jo sok kod.
+  De hogy ellenorizzuk, ameddig jutottunk, kikommenteljuk.
+  
+  dsfadfadfkadkjfdajfd
+  .sdfkjsadfkasdjfad
+  
+  */
+
+  std::cin.get();
+    return 0;
+}
+
