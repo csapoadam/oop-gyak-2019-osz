2367dce9b727a2d18aaa182ca6e1643476c6d4d3
Adam Csapo <csapo.adam@gmail.com>
2019-10-16 18:13:35 +0200

    use own exception class+

--- gyak06/solutionTable.h
+++ gyak06/solutionTable.h
@@ -4,6 +4,16 @@
 
 const int MAX_TABLE_SZ = 2;
 
+class GyakTableException : public std::exception {
+private:
+	std::string reason;
+public:
+	GyakTableException(const std::string& reason)
+		:
+		reason("Table error!! " + reason) {}
+	std::string getReason() { return reason; }
+};
+
 // Kovetelmenyek: sokfelekeppen meg lehetne oldani (pl. STL set)
 // de most fix meretu tombot hasznalunk
 // mi fogjuk shiftelni az elemeket a beszuraskor
@@ -15,9 +25,9 @@ private:
 public:
 	Table () : currentSize(0) {}
 	void tableInsert(TableItemType* newitem) {
-		if (currentSize + 1 == MAX_TABLE_SZ) {
+		if (currentSize == MAX_TABLE_SZ) {
 			// nincs tobb hely!
-			throw std::runtime_error("ERROR: table full!");
+			throw GyakTableException("table full!");
 		}
 		data[currentSize] = newitem;
 		currentSize++;
