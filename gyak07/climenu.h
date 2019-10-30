8a0a8462f8d252b04785588b208e248670c3fce9
Adam Csapo <csapo.adam@gmail.com>
2019-10-30 17:19:38 +0100

    meg tobb fun konstruktorokkal

--- gyak07/climenu.h
+++ gyak07/climenu.h
@@ -8,8 +8,15 @@ class CliMenuItem {
 private:
 	std::string itemText;
 	CliMenu* itemSubMenu;
+	void (*functionPointer)(); // main-ben levo exitFunction()
+	// alapjan tudjuk, hogy ilyen fv pointer kell ide
 public:
 	CliMenuItem(const std::string& text, CliMenu* submenu) {
+		itemText = text;
 		itemSubMenu = submenu;
+	}
+	CliMenuItem(const std::string& text, void (*fp)()) {
+		itemText = text;
+		functionPointer = fp;
 	}
 };
--- gyak07/main.cpp
+++ gyak07/main.cpp
@@ -4,8 +4,12 @@
 #include "climenu.h"
 
 void exitFunction() {
-	exit(EXIT_SUCCESS)
+	exit(EXIT_SUCCESS);
 }
+
+void menuDisplayFunction() {}
+
+void menuRefreshFunction() {}
 
 int main() {
 	CliMenu mainMenu;
@@ -16,10 +20,10 @@ int main() {
 	CliMenuItem item3("Help", &helpSubmenu); // help almenut jeleniti meg
 	CliMenuItem item4("Exit", &exitFunction);
 
-	mainMenu.addItem(item1);
-	mainMenu.addItem(item2);
-	mainMenu.addItem(item3);
-	mainMenu.addItem(item4);
+	mainMenu.addItem(&item1);
+	mainMenu.addItem(&item2);
+	mainMenu.addItem(&item3);
+	mainMenu.addItem(&item4);
 
 	CliMenuItem item5("About this restaurant", &about); // beolvassa az about-ot egy file-bol...
 	CliMenuItem item6("Back to main menu", &mainMenu);
@@ -43,5 +47,5 @@ int main() {
 	}
 
 	std::cin.get();
-    return 0;
+	return 0;
 }
